316aa4e8746a2954d20254a884fffbe16ccda615
Guy Lemon <glemon@Guys-Mac-Studio.attlocal.net>
2026-04-29 18:08:31 -0500

    chore(ios): initial commit

---
+++ GuyOS/GuyOS/GuyOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuyOSApp.swift
+//  GuyOS
+//
+//  Created by Guy Lemon on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct GuyOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
